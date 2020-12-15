b2c6f224c9a71d2415298aa68dead7133d5f880f
Dominic Padula <domopadula2192@gmail.com>
2020-12-15 10:42:25 -0700

    Forgot commits..through iteration 4

---
+++ lib/network.rb
@@ -0,0 +1,68 @@
+class Network
+  attr_reader:name,
+             :shows
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    @shows.map do |show|
+      show.characters.find_all do |character|
+          character.name == character.name.upcase &&
+        character.salary > 500_000
+      end
+    end.flatten
+  end
+
+
+  def actors_by_show
+    @shows.reduce({}) do |collector, show|
+      collector[show] = show.actors
+      collector
+    end
+  end
+
+  def character_shows(character_names)
+    actor_shows = []
+    @shows.each do |show|
+      if show.characters.each do |character|
+        character_names.include?(character.actor)
+        actor_shows << show
+        end
+      end
+    end
+    actor_shows.uniq 
+  end
+
+  def shows_by_actor
+    character_names = []
+    @shows.each do |show|
+      show.characters.each do |character|
+        character_names << character.actor
+      end
+    end
+      character_names.uniq.reduce({}) do |collector, name|
+      collector[name] = character_shows(character_names)
+      collector
+    end
+  end
+
+  def prolific_actors
+    actor_names = []
+    @shows.each do |show|
+      show.characters.each do |character|
+        actor_names << character.actor
+      end
+    end
+    actor_names.find_all do |name|
+      if (actor_names.count(name) > 1) == true
+        return [name]
+      end
+    end
+  end
+end
---
+++ lib/show.rb
@@ -0,0 +1,30 @@
+class Show
+  attr_reader:name,
+             :creator,
+             :characters
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    characters.sum do |character|
+      character.salary
+    end
+  end
+
+  def highest_paid_actor
+    characters.max_by do |character|
+      character.salary
+    end.actor
+  end
+
+  def actors
+    actor_names = []
+    characters.each do |character|
+      actor_names << character.actor
+    end
+    actor_names 
+  end
+end
